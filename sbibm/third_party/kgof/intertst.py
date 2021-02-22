877fe01df2fef96ba2eb637236331ce885a88ab9
bkmi <12955549+bkmi@users.noreply.github.com>
2021-02-22 11:32:15 +0100

    blackened code for ease of use

--- sbibm/third_party/kgof/intertst.py
+++ sbibm/third_party/kgof/intertst.py
@@ -8,11 +8,13 @@ freqopttest https://github.com/wittawatj/interpretable-test
 """
 
 from builtins import str
-__author__ = 'wittawat'
+
+__author__ = "wittawat"
 
 from abc import ABCMeta, abstractmethod
 import autograd
 import autograd.numpy as np
+
 # Require freqopttest https://github.com/wittawatj/interpretable-test
 import freqopttest.tst as tst
 import freqopttest.data as fdata
@@ -52,7 +54,10 @@ class GaussMETest(gof.GofTest):
         self.seed = seed
         ds = p.get_datasource()
         if ds is None:
-            raise ValueError('%s test requires a density p which implements get_datasource(', str(GaussMETest))
+            raise ValueError(
+                "%s test requires a density p which implements get_datasource(",
+                str(GaussMETest),
+            )
 
         # Construct the ME test
         metest = tst.MeanEmbeddingTest(test_locs, gwidth2, alpha=alpha)
@@ -74,10 +79,10 @@ class GaussMETest(gof.GofTest):
             # Run the two-sample test on p_sample and dat
             # Make a two-sample test data
             tst_data = fdata.TSTData(p_sample.data(), dat.data())
-            # Test 
+            # Test
             results = metest.perform_test(tst_data)
 
-        results['time_secs'] = t.secs
+        results["time_secs"] = t.secs
         return results
 
     def compute_stat(self, dat):
@@ -92,8 +97,9 @@ class GaussMETest(gof.GofTest):
         s = metest.compute_stat(tst_data)
         return s
 
-        
+
 # end GaussMETest
+
 
 class GaussMETestOpt(gof.GofTest):
     """
@@ -120,13 +126,16 @@ class GaussMETestOpt(gof.GofTest):
         """
         super(GaussMETestOpt, self).__init__(p, alpha)
         if tr_proportion <= 0 or tr_proportion >= 1:
-            raise ValueError('tr_proportion must be between 0 and 1 (exclusive)')
+            raise ValueError("tr_proportion must be between 0 and 1 (exclusive)")
         self.n_locs = n_locs
         self.tr_proportion = tr_proportion
         self.seed = seed
         ds = p.get_datasource()
         if ds is None:
-            raise ValueError('%s test requires a density p which implements get_datasource(', str(GaussMETest))
+            raise ValueError(
+                "%s test requires a density p which implements get_datasource(",
+                str(GaussMETest),
+            )
 
     def perform_test(self, dat, op=None, return_metest=False):
         """
@@ -138,36 +147,47 @@ class GaussMETestOpt(gof.GofTest):
         with util.ContextTimer() as t:
             metest, tr_tst_data, te_tst_data = self._get_metest_opt(dat, op)
 
-            # Run the two-sample test 
+            # Run the two-sample test
             results = metest.perform_test(te_tst_data)
 
-        results['time_secs'] = t.secs
+        results["time_secs"] = t.secs
         if return_metest:
-            results['metest'] = metest
+            results["metest"] = metest
         return results
 
     def _get_metest_opt(self, dat, op=None):
         seed = self.seed
         if op is None:
-            op = {'n_test_locs': self.n_locs, 'seed': seed+5, 'max_iter': 100, 
-                 'batch_proportion': 1.0, 'locs_step_size': 1.0, 
-                  'gwidth_step_size': 0.1, 'tol_fun': 1e-4, 'reg':1e-6}
+            op = {
+                "n_test_locs": self.n_locs,
+                "seed": seed + 5,
+                "max_iter": 100,
+                "batch_proportion": 1.0,
+                "locs_step_size": 1.0,
+                "gwidth_step_size": 0.1,
+                "tol_fun": 1e-4,
+                "reg": 1e-6,
+            }
         seed = self.seed
         alpha = self.alpha
         p = self.p
         # Draw sample from p. #sample to draw is the same as that of dat
         ds = p.get_datasource()
         p_sample = ds.sample(dat.sample_size(), seed=seed)
-        xtr, xte = p_sample.split_tr_te(tr_proportion=self.tr_proportion, seed=seed+18)
+        xtr, xte = p_sample.split_tr_te(
+            tr_proportion=self.tr_proportion, seed=seed + 18
+        )
         # ytr, yte are of type data.Data
-        ytr, yte = dat.split_tr_te(tr_proportion=self.tr_proportion, seed=seed+12)
+        ytr, yte = dat.split_tr_te(tr_proportion=self.tr_proportion, seed=seed + 12)
 
         # training and test data
         tr_tst_data = fdata.TSTData(xtr.data(), ytr.data())
         te_tst_data = fdata.TSTData(xte.data(), yte.data())
 
         # Train the ME test
-        V_opt, gw2_opt, _ = tst.MeanEmbeddingTest.optimize_locs_width(tr_tst_data, alpha, **op)
+        V_opt, gw2_opt, _ = tst.MeanEmbeddingTest.optimize_locs_width(
+            tr_tst_data, alpha, **op
+        )
         metest = tst.MeanEmbeddingTest(V_opt, gw2_opt, alpha)
         return metest, tr_tst_data, te_tst_data
 
@@ -177,4 +197,3 @@ class GaussMETestOpt(gof.GofTest):
         # Make a two-sample test data
         s = metest.compute_stat(te_tst_data)
         return s
-
--- sbibm/third_party/kgof/plot.py
+++ sbibm/third_party/kgof/plot.py
@@ -2,7 +2,8 @@
 
 from builtins import range
 from builtins import object
-__author__ = 'wittawat'
+
+__author__ = "wittawat"
 
 import sbibm.third_party.kgof.glo as glo
 import matplotlib
@@ -16,64 +17,61 @@ def get_func_tuples():
         (func_name used in the experiments, label name, plot line style)
     """
     func_tuples = [
-            ('job_fssdJ1q_med', 'FSSD-rand J1', 'r--^'),
-            ('job_fssdJ5q_med', 'FSSD-rand', 'r--^'),
-            ('job_fssdq_med', 'FSSD-rand', 'r--^'),
-
-            ('job_fssdJ1q_opt', 'FSSD-opt J1', 'r-s'),
-            ('job_fssdq_opt', 'FSSD-opt', 'r-s'),
-            ('job_fssdJ5q_opt', 'FSSD-opt', 'r-s'),
-            ('job_fssdJ5q_imq_optv', 'FSSD-IMQv', 'k-h'),
-            ('job_fssdJ5q_imqb1_optv', 'FSSD-IMQ-1', 'k--s'),
-            ('job_fssdJ5q_imqb2_optv', 'FSSD-IMQ-2', 'k-->'),
-            ('job_fssdJ5q_imqb3_optv', 'FSSD-IMQ-3', 'k-.*'),
-            ('job_fssdJ5q_imq_opt', 'FSSD-IMQ', 'y-x'),
-            ('job_fssdJ5q_imq_optbv', 'FSSD-IMQ-bv', 'y--d'),
-            ('job_fssdJ10q_opt', 'FSSD-opt', 'k-s'),
-
-            ('job_fssdJ5p_opt', 'FSSD-opt J5', 'm-s'),
-            ('job_fssdp_opt', 'FSSDp-opt', 'm-s'),
-            ('job_fssdJ10p_opt', 'FSSDp-opt J10', 'k-s'),
-
-            ('job_fssdJ1q_opt2', 'FSSD-opt2 J1', 'b-^'),
-            ('job_fssdJ5q_opt2', 'FSSD-opt2 J5', 'r-^'),
-            ('job_me_opt', 'ME-opt', 'b-d'),
-
-            ('job_kstein_med', 'KSD', 'g-o'),
-            ('job_kstein_imq', 'KSD-IMQ', 'c-*'),
-            ('job_lin_kstein_med', 'LKS', 'g-.h'),
-            ('job_mmd_med', 'MMD', 'm--^'),
-            ('job_mmd_opt', 'MMD-opt', 'm-<'),
-            ('job_mmd_dgauss_opt', 'MMD-dopt', 'y-<'),
-            ]
+        ("job_fssdJ1q_med", "FSSD-rand J1", "r--^"),
+        ("job_fssdJ5q_med", "FSSD-rand", "r--^"),
+        ("job_fssdq_med", "FSSD-rand", "r--^"),
+        ("job_fssdJ1q_opt", "FSSD-opt J1", "r-s"),
+        ("job_fssdq_opt", "FSSD-opt", "r-s"),
+        ("job_fssdJ5q_opt", "FSSD-opt", "r-s"),
+        ("job_fssdJ5q_imq_optv", "FSSD-IMQv", "k-h"),
+        ("job_fssdJ5q_imqb1_optv", "FSSD-IMQ-1", "k--s"),
+        ("job_fssdJ5q_imqb2_optv", "FSSD-IMQ-2", "k-->"),
+        ("job_fssdJ5q_imqb3_optv", "FSSD-IMQ-3", "k-.*"),
+        ("job_fssdJ5q_imq_opt", "FSSD-IMQ", "y-x"),
+        ("job_fssdJ5q_imq_optbv", "FSSD-IMQ-bv", "y--d"),
+        ("job_fssdJ10q_opt", "FSSD-opt", "k-s"),
+        ("job_fssdJ5p_opt", "FSSD-opt J5", "m-s"),
+        ("job_fssdp_opt", "FSSDp-opt", "m-s"),
+        ("job_fssdJ10p_opt", "FSSDp-opt J10", "k-s"),
+        ("job_fssdJ1q_opt2", "FSSD-opt2 J1", "b-^"),
+        ("job_fssdJ5q_opt2", "FSSD-opt2 J5", "r-^"),
+        ("job_me_opt", "ME-opt", "b-d"),
+        ("job_kstein_med", "KSD", "g-o"),
+        ("job_kstein_imq", "KSD-IMQ", "c-*"),
+        ("job_lin_kstein_med", "LKS", "g-.h"),
+        ("job_mmd_med", "MMD", "m--^"),
+        ("job_mmd_opt", "MMD-opt", "m-<"),
+        ("job_mmd_dgauss_opt", "MMD-dopt", "y-<"),
+    ]
     return func_tuples
+
 
 def set_default_matplotlib_options():
     # font options
     font = {
-    #     'family' : 'normal',
+        #     'family' : 'normal',
         #'weight' : 'bold',
-        'size'   : 30
+        "size": 30
     }
-    matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-
+    matplotlib.rc("font", **{"family": "serif", "serif": ["Computer Modern"]})
 
     # matplotlib.use('cairo')
-    matplotlib.rc('text', usetex=True)
-    matplotlib.rcParams['text.usetex'] = True
-    plt.rc('font', **font)
-    plt.rc('lines', linewidth=3, markersize=10)
+    matplotlib.rc("text", usetex=True)
+    matplotlib.rcParams["text.usetex"] = True
+    plt.rc("font", **font)
+    plt.rc("lines", linewidth=3, markersize=10)
     # matplotlib.rcParams['ps.useafm'] = True
     # matplotlib.rcParams['pdf.use14corefonts'] = True
 
-    matplotlib.rcParams['pdf.fonttype'] = 42
-    matplotlib.rcParams['ps.fonttype'] = 42
+    matplotlib.rcParams["pdf.fonttype"] = 42
+    matplotlib.rcParams["ps.fonttype"] = 42
+
 
 def get_func2label_map():
     # map: job_func_name |-> plot label
     func_tuples = get_func_tuples()
-    #M = {k:v for (k,v) in zip(func_names, labels)}
-    M = {k:v for (k,v,_) in func_tuples}
+    # M = {k:v for (k,v) in zip(func_names, labels)}
+    M = {k: v for (k, v, _) in func_tuples}
     return M
 
 
@@ -81,10 +79,10 @@ def func_plot_fmt_map():
     """
     Return a map from job function names to matplotlib plot styles 
     """
-    # line_styles = ['o-', 'x-',  '*-', '-_', 'D-', 'h-', '+-', 's-', 'v-', 
+    # line_styles = ['o-', 'x-',  '*-', '-_', 'D-', 'h-', '+-', 's-', 'v-',
     #               ',-', '1-']
     func_tuples = get_func_tuples()
-    M = {k:v for (k, _, v) in func_tuples}
+    M = {k: v for (k, _, v) in func_tuples}
     return M
 
 
@@ -93,6 +91,7 @@ class PlotValues(object):
     An object encapsulating values of a plot where there are many curves, 
     each corresponding to one method, with common x-axis values.
     """
+
     def __init__(self, xvalues, methods, plot_matrix):
         """
         xvalues: 1d numpy array of x-axis values
@@ -115,14 +114,18 @@ class PlotValues(object):
         plot_matrix = self.plot_matrix
 
         import tabulate
+
         # https://pypi.python.org/pypi/tabulate
         aug_table = np.hstack((np.array(methods)[:, np.newaxis], plot_matrix))
         return tabulate.tabulate(aug_table, xvalues, tablefmt=tablefmt)
 
+
 # end of class PlotValues
 
-def plot_prob_reject(ex, fname, func_xvalues, xlabel, func_title=None, 
-        return_plot_values=False):
+
+def plot_prob_reject(
+    ex, fname, func_xvalues, xlabel, func_title=None, return_plot_values=False
+):
     """
     plot the empirical probability that the statistic is above the threshold.
     This can be interpreted as type-1 error (when H0 is true) or test power 
@@ -139,48 +142,48 @@ def plot_prob_reject(ex, fname, func_xvalues, xlabel, func_title=None,
 
     Return loaded results
     """
-    #from IPython.core.debugger import Tracer 
-    #Tracer()()
+    # from IPython.core.debugger import Tracer
+    # Tracer()()
 
     results = glo.ex_load_result(ex, fname)
 
     def rej_accessor(jr):
-        rej = jr['test_result']['h0_rejected']
-        # When used with vectorize(), making the value float will make the resulting 
+        rej = jr["test_result"]["h0_rejected"]
+        # When used with vectorize(), making the value float will make the resulting
         # numpy array to be of float. nan values can be stored.
         return float(rej)
 
-    #value_accessor = lambda job_results: job_results['test_result']['h0_rejected']
+    # value_accessor = lambda job_results: job_results['test_result']['h0_rejected']
     vf_pval = np.vectorize(rej_accessor)
-    # results['job_results'] is a dictionary: 
+    # results['job_results'] is a dictionary:
     # {'test_result': (dict from running perform_test(te) '...':..., }
-    rejs = vf_pval(results['job_results'])
-    repeats, _, n_methods = results['job_results'].shape
+    rejs = vf_pval(results["job_results"])
+    repeats, _, n_methods = results["job_results"].shape
 
     # yvalues (corresponding to xvalues) x #methods
     mean_rejs = np.mean(rejs, axis=0)
-    #print mean_rejs
-    #std_pvals = np.std(rejs, axis=0)
-    #std_pvals = np.sqrt(mean_rejs*(1.0-mean_rejs))
+    # print mean_rejs
+    # std_pvals = np.std(rejs, axis=0)
+    # std_pvals = np.sqrt(mean_rejs*(1.0-mean_rejs))
 
     xvalues = func_xvalues(results)
 
-    #ns = np.array(results[xkey])
-    #te_proportion = 1.0 - results['tr_proportion']
-    #test_sizes = ns*te_proportion
+    # ns = np.array(results[xkey])
+    # te_proportion = 1.0 - results['tr_proportion']
+    # test_sizes = ns*te_proportion
     line_styles = func_plot_fmt_map()
     method_labels = get_func2label_map()
-    
-    func_names = [f.__name__ for f in results['method_job_funcs'] ]
+
+    func_names = [f.__name__ for f in results["method_job_funcs"]]
     plotted_methods = []
-    for i in range(n_methods):    
-        te_proportion = 1.0 - results['tr_proportion']
+    for i in range(n_methods):
+        te_proportion = 1.0 - results["tr_proportion"]
         fmt = line_styles[func_names[i]]
-        #plt.errorbar(ns*te_proportion, mean_rejs[:, i], std_pvals[:, i])
+        # plt.errorbar(ns*te_proportion, mean_rejs[:, i], std_pvals[:, i])
         method_label = method_labels[func_names[i]]
         plotted_methods.append(method_label)
         plt.plot(xvalues, mean_rejs[:, i], fmt, label=method_label)
-    '''
+    """
     else:
         # h0 is true 
         z = stats.norm.isf( (1-confidence)/2.0)
@@ -189,65 +192,76 @@ def plot_prob_reject(ex, fname, func_xvalues, xlabel, func_title=None,
             conf_iv = z*(phat*(1-phat)/repeats)**0.5
             #plt.errorbar(test_sizes, phat, conf_iv, fmt=line_styles[i], label=method_labels[i])
             plt.plot(test_sizes, mean_rejs[:, i], line_styles[i], label=method_labels[i])
-    '''
-            
-    ylabel = 'Rejection rate'
+    """
+
+    ylabel = "Rejection rate"
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
-    plt.xticks(np.hstack((xvalues) ))
-    
-    alpha = results['alpha']
-    plt.legend(loc='best')
-    title = '%s. %d trials. $\\alpha$ = %.2g.'%( results['prob_label'],
-            repeats, alpha) if func_title is None else func_title(results)
+    plt.xticks(np.hstack((xvalues)))
+
+    alpha = results["alpha"]
+    plt.legend(loc="best")
+    title = (
+        "%s. %d trials. $\\alpha$ = %.2g." % (results["prob_label"], repeats, alpha)
+        if func_title is None
+        else func_title(results)
+    )
     plt.title(title)
     plt.grid()
     if return_plot_values:
-        return results, PlotValues(xvalues=xvalues, methods=plotted_methods,
-                plot_matrix=mean_rejs.T)
+        return (
+            results,
+            PlotValues(
+                xvalues=xvalues, methods=plotted_methods, plot_matrix=mean_rejs.T
+            ),
+        )
     else:
         return results
-        
+
 
 def plot_runtime(ex, fname, func_xvalues, xlabel, func_title=None):
     results = glo.ex_load_result(ex, fname)
-    value_accessor = lambda job_results: job_results['time_secs']
+    value_accessor = lambda job_results: job_results["time_secs"]
     vf_pval = np.vectorize(value_accessor)
-    # results['job_results'] is a dictionary: 
+    # results['job_results'] is a dictionary:
     # {'test_result': (dict from running perform_test(te) '...':..., }
-    times = vf_pval(results['job_results'])
-    repeats, _, n_methods = results['job_results'].shape
+    times = vf_pval(results["job_results"])
+    repeats, _, n_methods = results["job_results"].shape
     time_avg = np.mean(times, axis=0)
     time_std = np.std(times, axis=0)
 
     xvalues = func_xvalues(results)
 
-    #ns = np.array(results[xkey])
-    #te_proportion = 1.0 - results['tr_proportion']
-    #test_sizes = ns*te_proportion
+    # ns = np.array(results[xkey])
+    # te_proportion = 1.0 - results['tr_proportion']
+    # test_sizes = ns*te_proportion
     line_styles = func_plot_fmt_map()
     method_labels = get_func2label_map()
-    
-    func_names = [f.__name__ for f in results['method_job_funcs'] ]
-    for i in range(n_methods):    
-        te_proportion = 1.0 - results['tr_proportion']
+
+    func_names = [f.__name__ for f in results["method_job_funcs"]]
+    for i in range(n_methods):
+        te_proportion = 1.0 - results["tr_proportion"]
         fmt = line_styles[func_names[i]]
-        #plt.errorbar(ns*te_proportion, mean_rejs[:, i], std_pvals[:, i])
+        # plt.errorbar(ns*te_proportion, mean_rejs[:, i], std_pvals[:, i])
         method_label = method_labels[func_names[i]]
-        plt.errorbar(xvalues, time_avg[:, i], yerr=time_std[:,i], fmt=fmt,
-                label=method_label)
-            
-    ylabel = 'Time (s)'
+        plt.errorbar(
+            xvalues, time_avg[:, i], yerr=time_std[:, i], fmt=fmt, label=method_label
+        )
+
+    ylabel = "Time (s)"
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
     plt.xlim([np.min(xvalues), np.max(xvalues)])
-    plt.xticks( xvalues, xvalues )
-    plt.legend(loc='best')
-    plt.gca().set_yscale('log')
-    title = '%s. %d trials. '%( results['prob_label'],
-            repeats ) if func_title is None else func_title(results)
+    plt.xticks(xvalues, xvalues)
+    plt.legend(loc="best")
+    plt.gca().set_yscale("log")
+    title = (
+        "%s. %d trials. " % (results["prob_label"], repeats)
+        if func_title is None
+        else func_title(results)
+    )
     plt.title(title)
-    #plt.grid()
+    # plt.grid()
     return results
 
 
@@ -262,8 +276,8 @@ def box_meshgrid(func, xbound, ybound, nx=50, ny=50):
     
     return XX, YY, ZZ where XX is a 2D nd-array of size nx x ny
     """
-    
-    # form a test location grid to try 
+
+    # form a test location grid to try
     minx, maxx = xbound
     miny, maxy = ybound
     loc0_cands = np.linspace(minx, maxx, nx)
@@ -272,16 +286,17 @@ def box_meshgrid(func, xbound, ybound, nx=50, ny=50):
     # nd1 x nd0 x 2
     loc3d = np.dstack((lloc0, lloc1))
     # #candidates x 2
-    all_loc2s = np.reshape(loc3d, (-1, 2) )
+    all_loc2s = np.reshape(loc3d, (-1, 2))
     # evaluate the function
     func_grid = func(all_loc2s)
     func_grid = np.reshape(func_grid, (ny, nx))
-    
+
     assert lloc0.shape[0] == ny
     assert lloc0.shape[1] == nx
     assert np.all(lloc0.shape == lloc1.shape)
-    
+
     return lloc0, lloc1, func_grid
+
 
 def get_density_cmap():
     """
@@ -291,7 +306,7 @@ def get_density_cmap():
     Varying from white (low) to red (high).
     """
     # Add completely white color to Reds colormap in Matplotlib
-    list_colors = plt.cm.datad['Reds']
+    list_colors = plt.cm.datad["Reds"]
     list_colors = list(list_colors)
     list_colors.insert(0, (1, 1, 1))
     list_colors.insert(0, (1, 1, 1))
