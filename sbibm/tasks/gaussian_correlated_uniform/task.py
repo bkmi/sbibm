bcfc9d0661fd85bd44908aa25638f8a4eb63e703
bkmi <12955549+bkmi@users.noreply.github.com>
2021-03-08 19:33:14 +0100

    added a correlated task and updated the truncnorm behavior

--- sbibm/tasks/gaussian_correlated_uniform/task.py
+++ sbibm/tasks/gaussian_correlated_uniform/task.py
@@ -1,7 +1,6 @@
-# pylint: disable=no-member, not-callable
 import logging
 from pathlib import Path
-from typing import Callable, List, Optional, Union
+from typing import Callable, Optional
 
 import pandas as pd
 import pyro
@@ -9,66 +8,52 @@ import scipy.stats
 import torch
 from pyro import distributions as pdist
 
+from sbibm.tasks.gaussian_linear_uniform.task import TruncNorm
 from sbibm.tasks.simulator import Simulator
 from sbibm.tasks.task import Task
 from sbibm.utils.io import get_tensor_from_csv
 
 
-class TruncNorm(object):
+def get_correlated_covariance_matrix(dim: int, correlation: float) -> torch.Tensor:
+    covariance_matrix = torch.eye(dim)
+    covariance_matrix[torch.triu_indices(dim, dim, 1).unbind()] = correlation
+    covariance_matrix[torch.tril_indices(dim, dim, -1).unbind()] = correlation
+    return covariance_matrix
+
+
+def get_random_rotation(dim: int, device=None, dtype=None) -> torch.Tensor:
+    return torch.from_numpy(scipy.stats.special_ortho_group(dim).rvs()).to(
+        device=device, dtype=dtype
+    )
+
+
+def rotate_matrix(matrix: torch.Tensor, rotation: torch.Tensor) -> torch.Tensor:
+    return rotation @ matrix @ rotation.T
+
+
+def normalize_covariance_matrix(covariance_matrix: torch.Tensor) -> torch.Tensor:
+    return covariance_matrix / torch.trace(covariance_matrix)
+
+
+def get_rotated_normalized_covariance_matrix(
+    dim: int, correlation: float
+) -> torch.Tensor:
+    covariance_matrix = get_correlated_covariance_matrix(dim, correlation)
+    rotation = get_random_rotation(
+        dim, device=covariance_matrix.device, dtype=covariance_matrix.dtype
+    )
+
+    normed_covariance_matrix = normalize_covariance_matrix(covariance_matrix)
+    return rotate_matrix(normed_covariance_matrix, rotation)
+
+
+class GaussianCorrelatedUniform(Task):
     def __init__(
         self,
-        mean: torch.Tensor,
-        low: float,
-        high: float,
-        scale: Union[float, torch.Tensor],
-    ):
-        self.mean = self._1d_tensor_to_list(mean)
-        self.dim = len(self.mean)
-
-        if isinstance(scale, torch.Tensor):
-            self.scale = self._1d_tensor_to_list(scale)
-        elif isinstance(scale, float):
-            self.scale = [scale] * self.dim
-        else:
-            raise NotImplementedError
-        assert self.dim == len(self.scale)
-
-        self.a = [(low - x) / s for x, s in zip(self.mean, self.scale)]
-        self.b = [(high - x) / s for x, s in zip(self.mean, self.scale)]
-        assert self.dim == len(self.a)
-        assert self.dim == len(self.b)
-
-        self.dists = [
-            scipy.stats.truncnorm(a=a, b=b, loc=m, scale=s)
-            for a, b, m, s in zip(self.a, self.b, self.mean, self.scale)
-        ]
-        assert self.dim == len(self.dists)
-
-    @staticmethod
-    def _1d_tensor_to_list(tensor: torch.Tensor) -> List[float]:
-        if tensor.ndim == 1:
-            tensor = tensor.tolist()
-        elif tensor.ndim == 2:
-            assert tensor.size(0) == 1
-            tensor = tensor[0].tolist()
-        else:
-            raise NotImplementedError
-        return tensor
-
-    def sample(self) -> torch.Tensor:
-        sample = [d.rvs() for d in self.dists]
-        return torch.tensor(sample).reshape(1, -1).to(torch.float32)
-
-    def log_prob(self, sample: torch.Tensor) -> torch.Tensor:
-        sample_list = self._1d_tensor_to_list(sample)
-        log_prob = sum([d.logpdf(x) for d, x in zip(self.dists, sample_list)])
-        print(log_prob)
-        return torch.tensor(log_prob, device=sample.device, dtype=sample.dtype)
-
-
-class GaussianLinearUniform(Task):
-    def __init__(
-        self, dim: int = 10, prior_bound: float = 1.0, simulator_scale: float = 0.1
+        dim: int = 10,
+        prior_bound: float = 1.0,
+        simulator_scale: float = 0.1,
+        simulator_correlation: float = 0.9,
     ):
         """Gaussian Linear Uniform
 
@@ -78,12 +63,13 @@ class GaussianLinearUniform(Task):
             dim: Dimensionality of parameters and data.
             prior_bound: Prior is uniform in [-prior_bound, +prior_bound].
             simulator_scale: Standard deviation of noise in simulator.
+            simulator_correlation: off diagonal value in correlation matrix.
         """
         super().__init__(
             dim_parameters=dim,
             dim_data=dim,
             name=Path(__file__).parent.name,
-            name_display="Gaussian Linear Uniform",
+            name_display="Gaussian Correlated Uniform",
             num_observations=10,
             num_posterior_samples=10000,
             num_reference_posterior_samples=10000,
@@ -98,10 +84,14 @@ class GaussianLinearUniform(Task):
 
         self.prior_dist = pdist.Uniform(**self.prior_params).to_event(1)
 
+        self.simulator_correlation = simulator_correlation
         self.simulator_scale = simulator_scale
         self.simulator_params = {
             "precision_matrix": torch.inverse(
-                simulator_scale * torch.eye(self.dim_parameters),
+                simulator_scale
+                * get_rotated_normalized_covariance_matrix(
+                    self.dim_parameters, simulator_correlation
+                ),
             )
         }
 
@@ -144,6 +134,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "observation.csv"
         )
@@ -156,6 +147,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "reference_posterior_samples.csv.bz2"
         )
@@ -168,6 +160,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "true_parameters.csv"
         )
@@ -180,6 +173,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "observation_seed.csv"
         )
@@ -192,6 +186,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "observation.csv"
         )
@@ -205,6 +200,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "observation_seed.csv"
         )
@@ -223,6 +219,7 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "reference_posterior_samples.csv.bz2"
         )
@@ -238,74 +235,12 @@ class GaussianLinearUniform(Task):
             / "files"
             / f"dim_{self.dim_parameters:04d}"
             / f"scale_{self.simulator_scale:.0e}"
+            / f"correlation_{self.simulator_correlation:.0e}"
             / f"num_observation_{num_observation}"
             / "true_parameters.csv"
         )
         path.parent.mkdir(parents=True, exist_ok=True)
         self.save_parameters(path, true_parameters)
-
-    # def _sample_reference_posterior(
-    #     self,
-    #     num_samples: int,
-    #     num_observation: Optional[int] = None,
-    #     observation: Optional[torch.Tensor] = None,
-    # ) -> torch.Tensor:
-    #     """Sample reference posterior for given observation
-
-    #     Uses closed form solution with rejection sampling
-
-    #     Args:
-    #         num_samples: Number of samples to generate
-    #         num_observation: Observation number
-    #         observation: Instead of passing an observation number, an observation may be
-    #             passed directly
-
-    #     Returns:
-    #         Samples from reference posterior
-    #     """
-    #     assert not (num_observation is None and observation is None)
-    #     assert not (num_observation is not None and observation is not None)
-
-    #     if num_observation is not None:
-    #         observation = self.get_observation(num_observation=num_observation)
-
-    #     log = logging.getLogger(__name__)
-
-    #     reference_posterior_samples = []
-
-    # sampling_dist = pdist.MultivariateNormal(
-    #     loc=observation, precision_matrix=self.simulator_params["precision_matrix"],
-    # )
-
-    #     # Reject samples outside of prior bounds
-    #     counter = 0
-    #     while len(reference_posterior_samples) < num_samples:
-    #         counter += 1
-    #         sample = sampling_dist.sample()
-    #         if not torch.isinf(self.prior_dist.log_prob(sample).sum()):
-    #             reference_posterior_samples.append(sample)
-
-    #     reference_posterior_samples = torch.cat(reference_posterior_samples)
-    #     acceptance_rate = float(num_samples / counter)
-
-    #     log.info(
-    #         f"Acceptance rate for observation {num_observation}: {acceptance_rate}"
-    #     )
-
-    #     return reference_posterior_samples
-
-    def _get_scipy_truncnorm(
-        self, observation: torch.Tensor
-    ) -> scipy.stats.rv_continuous:
-        low = self.prior_params["low"][0].numpy()
-        high = self.prior_params["high"][0].numpy()
-        scale = 1 / self.simulator_params["precision_matrix"][0, 0].numpy()
-        return TruncNorm(
-            mean=observation,
-            low=low,
-            high=high,
-            scale=scale,
-        )
 
     def _sample_reference_posterior(
         self,
@@ -315,7 +250,7 @@ class GaussianLinearUniform(Task):
     ) -> torch.Tensor:
         """Sample reference posterior for given observation
 
-        Uses closed form solution using a truncated normal
+        Uses closed form solution with rejection sampling
 
         Args:
             num_samples: Number of samples to generate
@@ -336,9 +271,12 @@ class GaussianLinearUniform(Task):
 
         reference_posterior_samples = []
 
-        sampling_dist = self._get_scipy_truncnorm(observation)
+        sampling_dist = pdist.MultivariateNormal(
+            loc=observation,
+            precision_matrix=self.simulator_params["precision_matrix"],
+        )
 
-        # Reject samples outside of prior bounds, still required even with trunc norm
+        # Reject samples outside of prior bounds
         counter = 0
         while len(reference_posterior_samples) < num_samples:
             counter += 1
@@ -355,6 +293,92 @@ class GaussianLinearUniform(Task):
 
         return reference_posterior_samples
 
+    #########################################
+    # below ought to work in higher dim but
+    # assigns zero probability to an observation
+    # leading to an infinite domination_constant
+    #########################################
+
+    # def _get_scipy_truncnorm(
+    #     self, observation: torch.Tensor
+    # ) -> scipy.stats.rv_continuous:
+    #     low = self.prior_params["low"][0].numpy()
+    #     high = self.prior_params["high"][0].numpy()
+
+    #     # This seems to underestimate in some dimensions
+    #     evals, _ = torch.symeig(self.simulator_params["precision_matrix"])
+    #     scale = 1 / evals
+
+    #     # scale = 1 / torch.trace(self.simulator_params["precision_matrix"]).numpy()
+    #     return TruncNorm(
+    #         mean=observation,
+    #         low=low,
+    #         high=high,
+    #         scale=scale,
+    #     )
+
+    # def _sample_reference_posterior(
+    #     self,
+    #     num_samples: int,
+    #     num_observation: Optional[int] = None,
+    #     observation: Optional[torch.Tensor] = None,
+    # ) -> torch.Tensor:
+    #     """Sample reference posterior for given observation
+
+    #     Uses closed form solution using a truncated normal
+
+    #     Args:
+    #         num_samples: Number of samples to generate
+    #         num_observation: Observation number
+    #         observation: Instead of passing an observation number, an observation may be
+    #             passed directly
+
+    #     Returns:
+    #         Samples from reference posterior
+    #     """
+    #     assert not (num_observation is None and observation is None)
+    #     assert not (num_observation is not None and observation is not None)
+
+    #     if num_observation is not None:
+    #         observation = self.get_observation(num_observation=num_observation)
+
+    #     log = logging.getLogger(__name__)
+
+    #     reference_posterior_samples = []
+
+    #     sampling_dist = self._get_scipy_truncnorm(observation)
+    #     posterior_dist = pdist.MultivariateNormal(
+    #         loc=observation,
+    #         precision_matrix=self.simulator_params["precision_matrix"],
+    #     )
+    #     domination_constant = posterior_dist.log_prob(observation).item() - sampling_dist.log_prob(observation)
+    #     # breakpoint()
+    #     # assert domination_constant >= 0.0
+    #     assert not torch.isinf(domination_constant)
+
+    #     counter = 0
+    #     while len(reference_posterior_samples) < num_samples:
+    #         counter += 1
+    #         sample = sampling_dist.sample()
+    #         if not torch.isinf(self.prior_dist.log_prob(sample).sum()):
+    #             # Reject samples outside of prior bounds
+    #             chance = posterior_dist.log_prob(sample) - (domination_constant + sampling_dist.log_prob(sample))
+    #             # print(chance)
+    #             # print(torch.rand(1).log().item())
+    #             breakpoint()
+    #             if torch.rand(1).log().item() < chance:
+    #                 breakpoint()
+    #                 reference_posterior_samples.append(sample)
+
+    #     reference_posterior_samples = torch.cat(reference_posterior_samples)
+    #     acceptance_rate = float(num_samples / counter)
+
+    #     log.info(
+    #         f"Acceptance rate for observation {num_observation}: {acceptance_rate}"
+    #     )
+
+    #     return reference_posterior_samples
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -363,10 +387,12 @@ if __name__ == "__main__":
     parser.add_argument("--n_jobs", type=int, default=-1)
     parser.add_argument("--dim", type=int, default=10)
     parser.add_argument("--simulator_scale", type=float, default=0.1)
+    parser.add_argument("--simulator_correlation", type=float, default=0.9)
     args = parser.parse_args()
 
-    task = GaussianLinearUniform(
+    task = GaussianCorrelatedUniform(
         dim=args.dim,
         simulator_scale=args.simulator_scale,
+        simulator_correlation=args.simulator_correlation,
     )
     task._setup(n_jobs=args.n_jobs)
