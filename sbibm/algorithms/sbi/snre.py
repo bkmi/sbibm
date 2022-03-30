4d02c91abb1fafb74fae96c19def180d2147266e
bkmi <12955549+bkmi@users.noreply.github.com>
2022-03-30 17:15:58 +0200

    added whats necesary to run nre

--- sbibm/algorithms/sbi/snre.py
+++ sbibm/algorithms/sbi/snre.py
@@ -2,6 +2,7 @@ import logging
 import math
 from typing import Any, Dict, Optional, Tuple
 
+import cnre.data
 import torch
 from sbi import inference as inference
 from sbi.utils.get_nn_models import classifier_nn
@@ -19,14 +20,18 @@ def run(
     num_samples: int,
     num_simulations: int,
     num_observation: Optional[int] = None,
+    training_samples_root: Optional[str] = None,
     observation: Optional[torch.Tensor] = None,
     num_rounds: int = 10,
     neural_net: str = "resnet",
     hidden_features: int = 50,
+    num_blocks: int = 2,
+    use_batch_norm: bool = True,
     simulation_batch_size: int = 1000,
     training_batch_size: int = 10000,
     num_atoms: int = 10,
     automatic_transforms_enabled: bool = True,
+    sample_with: str = "mcmc",
     mcmc_method: str = "slice_np_vectorized",
     mcmc_parameters: Dict[str, Any] = {
         "num_chains": 100,
@@ -40,6 +45,7 @@ def run(
     z_score_theta: bool = True,
     variant: str = "B",
     max_num_epochs: Optional[int] = None,
+    stop_after_epochs: int = 2 ** 31 - 1,
 ) -> Tuple[torch.Tensor, int, Optional[torch.Tensor]]:
     """Runs (S)NRE from `sbi`
 
@@ -100,6 +106,8 @@ def run(
     classifier = classifier_nn(
         model=neural_net.lower(),
         hidden_features=hidden_features,
+        num_blocks=num_blocks,
+        use_batch_norm=use_batch_norm,
         z_score_x=z_score_x,
         z_score_theta=z_score_theta,
     )
@@ -117,30 +125,46 @@ def run(
     posteriors = []
     proposal = prior
     mcmc_parameters["warmup_steps"] = 25
-    mcmc_parameters["enable_transform"] = False  # NOTE: Disable `sbi` auto-transforms, since `sbibm` does its own
+    # mcmc_parameters["enable_transform"] = False  # NOTE: Disable `sbi` auto-transforms, since `sbibm` does its own
 
     for r in range(num_rounds):
-        theta, x = inference.simulate_for_sbi(
-            simulator,
-            proposal,
-            num_simulations=num_simulations_per_round,
-            simulation_batch_size=simulation_batch_size,
-        )
+        if training_samples_root is None:
+            log.info(f"simulating round {r} training samples")
+            theta, x = inference.simulate_for_sbi(
+                simulator,
+                proposal,
+                num_simulations=num_simulations_per_round,
+                simulation_batch_size=simulation_batch_size,
+            )
+        elif training_samples_root is not None and num_rounds == 1:
+            log.info(f"loading training samples from: {training_samples_root}")
+            theta, x = cnre.data.load_training_samples(
+                task.name, num_simulations, training_samples_root
+            )
+        else:
+            raise NotImplementedError(
+                "Cannot load preloaded training samples and learn sequentially."
+            )
 
         density_estimator = inference_method.append_simulations(
             theta, x, from_round=r
         ).train(
             training_batch_size=training_batch_size,
-            retrain_from_scratch_each_round=False,
+            retrain_from_scratch=False,
             discard_prior_samples=False,
             show_train_summary=True,
             max_num_epochs=max_num_epochs,
+            stop_after_epochs=stop_after_epochs,
             **inference_method_kwargs,
         )
         if r > 1:
             mcmc_parameters["init_strategy"] = "latest_sample"
         posterior = inference_method.build_posterior(
-            density_estimator, mcmc_method=mcmc_method, mcmc_parameters=mcmc_parameters
+            density_estimator,
+            sample_with=sample_with,
+            mcmc_method=mcmc_method,
+            mcmc_parameters=mcmc_parameters,
+            enable_transform=False,  # NOTE: Disable `sbi` auto-transforms, since `sbibm` does its own
         )
         # Copy hyperparameters, e.g., mcmc_init_samples for "latest_sample" strategy.
         if r > 0:
@@ -150,8 +174,12 @@ def run(
 
     posterior = wrap_posterior(posteriors[-1], transforms)
 
-    assert simulator.num_simulations == num_simulations
+    if training_samples_root is None:
+        assert simulator.num_simulations == num_simulations
+        checked_num_simulations = simulator.num_simulations
+    else:
+        checked_num_simulations = num_simulations
 
     samples = posterior.sample((num_samples,)).detach()
 
-    return samples, simulator.num_simulations, None
+    return samples, checked_num_simulations, None
