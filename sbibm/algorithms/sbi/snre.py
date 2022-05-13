5b4138eea2b98e5f863865756509862efdb2f287
bkmi <12955549+bkmi@users.noreply.github.com>
2022-05-13 18:45:51 +0200

    removed dead kwargs

--- sbibm/algorithms/sbi/snre.py
+++ sbibm/algorithms/sbi/snre.py
@@ -39,8 +39,6 @@ def run(
         "thin": 10,
         "warmup_steps": 100,
         "init_strategy": "sir",
-        "sir_batch_size": 1000,
-        "sir_num_batches": 100,
     },
     z_score_x: bool = True,
     z_score_theta: bool = True,
